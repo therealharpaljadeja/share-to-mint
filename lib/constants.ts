11d1cde3c5ab56b06d995304246f6f3f91a23531
Harpalsinh Jadeja <jadejaharpal14@gmail.com>
2025-06-19 19:34:44 +0530

    fix: vercel production url env variable

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,7 @@
 export const MESSAGE_EXPIRATION_TIME = 1000 * 60 * 60 * 24 * 30; // 30 day
 export const APP_URL =
     process.env.VERCEL_ENV === "production"
-        ? `https://${process.env.VERCEL_URL}`
+        ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
         : process.env.NEXT_PUBLIC_URL ?? "http://localhost:3000";
 if (!APP_URL) {
     throw new Error("NEXT_PUBLIC_URL is not set");
