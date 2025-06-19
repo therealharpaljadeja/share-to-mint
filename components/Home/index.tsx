a012d987413f69ee6e981a9053c6cec30a666d6e
Harpal Jadeja <jadejaharpal14@gmail.com>
2025-06-19 19:14:31 +0530

    Initial commit

---
+++ components/Home/index.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { FarcasterActions } from '@/components/Home/FarcasterActions'
+import { User } from '@/components/Home/User'
+import { WalletActions } from '@/components/Home/WalletActions'
+
+export function Demo() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center p-4 space-y-8">
+      <h1 className="text-3xl font-bold text-center">
+        Monad Farcaster MiniApp Template
+      </h1>
+      <div className="w-full max-w-4xl space-y-6">
+        <User />
+        <FarcasterActions />
+        <WalletActions />
+      </div>
+    </div>
+  )
+}
