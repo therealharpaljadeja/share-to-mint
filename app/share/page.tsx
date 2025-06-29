512761c1af3a9327ebc8c243b7a66ba92690e7dd
Harpalsinh Jadeja <jadejaharpal14@gmail.com>
2025-06-29 17:17:49 +0530

    refactor: replace ShareContent component with Coin component in SharePage

--- app/share/page.tsx
+++ app/share/page.tsx
@@ -1,14 +1,14 @@
 // This is a server component by default
 import { Suspense } from "react";
-import ShareContent from "../../components/Share/ShareContent";
 import Loading from "@/components/screens/coin/Loading";
+import Coin from "@/components/screens/coin";
 
 export default function SharePage() {
   return (
     <Suspense fallback={
      <Loading />
     }>
-      <ShareContent />
+      <Coin />
     </Suspense>
   );
 }
